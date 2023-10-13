93efb84b95b1d01c882a7e7a58c80065cdb7c7a9
IronmanHUB4VF <122427542+IronmanHUB4VF@users.noreply.github.com>
2023-10-13 17:04:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['24736263'])
-API_HASH = environ['4d53732917b73a6bb89c3b2f2f7b0902']
-BOT_TOKEN = environ['6294506492:AAHWWWbk1TmdFCvLDGcHRgPJrpyoHEDwoYU']
+API_ID = int(environ.get('API_ID', '24736263'))
+API_HASH = environ.get('API_HASH', '4d53732917b73a6bb89c3b2f2f7b0902')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6294506492:AAHWWWbk1TmdFCvLDGcHRgPJrpyoHEDwoYU')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
